46d369e892741ce26166236d93e20450a94fd5a0
bissmepj <bissmepj@rose-hulman.edu>
2017-11-27 09:42:58 -0500

    Completed the TODOs

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Phil Bissmeyer.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
@@ -28,3 +28,22 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+fast_turtle = rg.SimpleTurtle('turtle')
+slow_turtle = rg.SimpleTurtle('turtle')
+fast_turtle.speed = 10
+slow_turtle.speed = 5
+slow_turtle.right(180)
+
+for k in range(15):
+    x = (10*k)+10
+    fast_turtle.draw_circle(x)
+    fast_turtle.right(90)
+    slow_turtle.draw_square(x)
+    slow_turtle.left(90)
+
+window.close_on_mouse_click()
